f76db9f8966543c2939c0de8f745b501973b34be
Cole <deoncole77@gmail.com>
2022-05-01 17:58:30 -0400

    create post model

---
+++ models/Post.js
@@ -0,0 +1,46 @@
+// require the model and datatypes form sequelize
+const { Model, DataTypes } = require('sequelize');
+// require the connection
+const sequelize = require('../config/connection');
+
+// model to create the Post table
+class Post extends Model{}
+
+// create the table
+Post.init(
+    {
+        id: {
+            type: DataTypes.INTEGER,
+            allowNull: false,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        title: {
+            type: DataTypes.STRING,
+            allowNull: false
+        },
+        post_url: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                isUrl: true
+            }
+        },
+        user_id:{
+            type: DataTypes.INTEGER,
+            references: {
+                model: 'user',
+                key: 'id'
+            }
+        }
+    },
+    {
+        sequelize,
+        freezeTableName: true,
+        underscored: true,
+        modelName: 'post'
+    }
+);
+
+// export the module
+module.exports = Post;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 // require the Models to access the data
 const User = require('./User');
+const Post = require('./Post');
 
 // export the modelnpm start objects
-module.exports = {User}
+module.exports = {User, Post}
