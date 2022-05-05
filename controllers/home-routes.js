9eeb0631831dbb6ef8385b23db114b49d1edf804
Cole <deoncole77@gmail.com>
2022-05-04 23:51:25 -0400

    check login and update post

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -73,7 +73,7 @@ router.get('/post/:id', (req,res) => {
             const post = dbPostData.get({ plain: true });
 
             // pass data to template
-            res.render('singlepost', { post });
+            res.render('singlepost', { post, loggedIn: req.session.loggedIn });
         })
         .catch(err => {
             console.log(err);
